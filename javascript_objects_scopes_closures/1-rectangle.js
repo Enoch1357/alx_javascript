1454d787f2c9ca29d6b077ebcb7a242f6e97961d
Enoch1357 <enochmensah1357@gmail.com>
2023-09-27 19:26:00 +0000

    1-rectangle.js update

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,9 @@
 #! /usr/bin/python3
 // This script contains a class 'Rectangle' which takes 2 arguments 'w' and 'h'
-module.exports = Rectangle;
 class Rectangle {
     constructor(w, h) {
       this.width = w;
       this.height = h;
     }
   }
+module.exports = Rectangle;
