6d3afca6ac58588b85b1eb2d3f2d5ac2b18eba0f
Enoch1357 <enochmensah1357@gmail.com>
2023-09-27 19:28:03 +0000

    2-rectangle.js pushed

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+// This script contains a class 'Rectangle' with two attributes width and height
+class Rectangle {
+    constructor(w, h) {
+      if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+        return {};
+      }
+  
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  module.exports = Rectangle;
