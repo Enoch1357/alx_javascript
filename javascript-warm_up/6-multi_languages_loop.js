503e53386454363bfa48d5ed160c4178c064ec57
Enoch1357 <enochmensah1357@gmail.com>
2023-09-27 18:21:38 +0000

    6-multi_languages_loop.js pushed

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+// This script prints 3 lines: but by using an array of string and a loop
+const text = [
+    "C is fun",
+    "Python is cool",
+    "JavaScript is amazing"
+  ];
+  
+  for (let i = 0; i < text.length; i++) {
+    console.log(text[i]);
+  }
+  
