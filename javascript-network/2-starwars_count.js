5ccf2f58879446bee4db747789059bda176a1518
Enoch1357 <enochmensah1357@gmail.com>
2023-09-27 21:19:59 +0000

    2-starwars_count.js pushed

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,26 +1,33 @@
 #!/usr/bin/node
 // This script prints the number of movies where the character “Wedge Antilles” is present.
-const request = require('request');
-
-// Define the Star Wars API endpoint
-const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
-const characterId = 18; // Wedge Antilles character ID
-
-// Make a GET request to the API
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error(`Request failed with status code: ${response.statusCode}`);
-  } else {
-    const movies = JSON.parse(body);
-    const count = movies.results.reduce((total, movie) => {
-      if (movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-        return total + 1;
+// Your JSON data
+const data = {
+    "count": 7,
+    "next": null,
+    "previous": null,
+    "results": [
+      // ... (your movie data)
+    ]
+  };
+  
+  // Function to count movies with a specific character
+  function countMoviesWithCharacter(data, characterUrl) {
+    let count = 0;
+    data.results.forEach(movie => {
+      const characters = movie.characters;
+      if (characters.includes(characterUrl)) {
+        count++;
       }
-      return total;
-    }, 0);
-
-    console.log(`${count}`);
+    });
+    return count;
   }
-});
+  
+  // Define the character you want to search for (e.g., "Wedge Antilles")
+  const characterToSearch = "http://swapi.co/api/people/18/";
+  
+  // Count movies with the specified character
+  const numMovies = countMoviesWithCharacter(data, characterToSearch);
+  
+  // Print the result
+  console.log(`${numMovies}`);
+  
