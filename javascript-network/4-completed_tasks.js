7e95e066e21dc427c8aa4425b019698010f774b1
Enoch1357 <enochmensah1357@gmail.com>
2023-09-27 20:59:38 +0000

    4-completed_tasks.js update

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -2,31 +2,38 @@
 // This script computes the number of tasks completed by user id.
 const request = require('request');
 
-const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+// Define the API URL
+const apiUrl = process.argv[2]; // Read the API URL from command line arguments
 
-const userId = process.argv[2];
-
-if (!userId) {
-  console.error('Usage: node script.js <user-id>');
+if (!apiUrl) {
+  console.error('Usage: node 4-completed_tasks.js <api-url>');
   process.exit(1);
 }
 
+// Make a GET request to the API URL
 request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
   } else if (response.statusCode !== 200) {
     console.error(`Request failed with status code: ${response.statusCode}`);
   } else {
+    // Parse the JSON response
     const tasks = JSON.parse(body);
 
-    let numCompletedTasks = 0;
+    // Initialize an object to store the count of completed tasks for each user
+    const completedTaskCounts = {};
 
+    // Loop through tasks and count completed tasks for each user
     for (const task of tasks) {
-      if (task.userId === parseInt(userId) && task.completed) {
-        numCompletedTasks++;
+      if (task.completed) {
+        if (completedTaskCounts[task.userId]) {
+          completedTaskCounts[task.userId]++;
+        } else {
+          completedTaskCounts[task.userId] = 1;
+        }
       }
     }
 
-    console.log(`User ${userId} has completed ${numCompletedTasks} tasks.`);
+    console.log(completedTaskCounts);
   }
 });
