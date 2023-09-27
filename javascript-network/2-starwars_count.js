0952cc3f3150da93bbd4d5807cfe3576c48f386a
Enoch1357 <enochmensah1357@gmail.com>
2023-09-27 21:59:12 +0000

    2-starwars_count.js update

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -2,37 +2,25 @@
 // This script prints the number of movies where the character “Wedge Antilles” is present.
 const request = require('request');
 
-// Check if the API URL is provided as an argument
-const apiUrl = process.argv[2];
+// Define the Star Wars API endpoint
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
+const characterId = 18; // Wedge Antilles character ID
 
-if (!apiUrl) {
-  console.error('Please provide the API URL as an argument.');
-  process.exit(1);
-}
+// Make a GET request to the API
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error(`Request failed with status code: ${response.statusCode}`);
+  } else {
+    const movies = JSON.parse(body);
+    const count = movies.results.reduce((total, movie) => {
+      if (movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+        return total + 1;
+      }
+      return total;
+    }, 0);
 
-// Character ID for "Wedge Antilles"
-const characterId = 18;
-
-// Function to count movies with the specified character
-function countMoviesWithCharacter(apiUrl, characterId) {
-  request(apiUrl, (error, response, body) => {
-    if (error) {
-      console.error('Error:', error);
-      process.exit(1);
-    }
-
-    if (response.statusCode !== 200) {
-      console.error(`API returned a non-200 status code: ${response.statusCode}`);
-      process.exit(1);
-    }
-
-    try {
-      const data = JSON.parse(body);
-      const movies = data.results.filter(movie => movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
-      console.log(movies.length);
-    } catch (parseError) {
-      console.error('Error parsing API response:', parseError);
-      process.exit(1);
-    }
-  });
-}
+    console.log(`Number of movies with Wedge Antilles: ${count}`);
+  }
+});
