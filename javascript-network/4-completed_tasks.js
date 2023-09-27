92915adc314004ccc1b1f086bca6ae3aa9026afd
Enoch1357 <enochmensah1357@gmail.com>
2023-09-27 20:42:34 +0000

    4-completed_tasks.js pushed

---
+++ javascript-network/4-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+// This script computes the number of tasks completed by user id.
+const request = require('request');
+
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+const userId = process.argv[2];
+
+if (!userId) {
+  console.error('Usage: node script.js <user-id>');
+  process.exit(1);
+}
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error(`Request failed with status code: ${response.statusCode}`);
+  } else {
+    const tasks = JSON.parse(body);
+
+    const completedTasks = tasks.filter((task) => task.userId === parseInt(userId) && task.completed);
+
+    const numCompletedTasks = completedTasks.length;
+
+    console.log(`User ${userId} has completed ${numCompletedTasks} tasks.`);
+  }
+});
