c7787160b6a7be3fd3d026611aefe99a519aceb3
Enoch1357 <enochmensah1357@gmail.com>
2023-09-27 18:17:00 +0000

    1-multi_languages.js pushed

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+// This script prints out three lines of strings
+const text_1 = "C is fun";
+const text_2 = "Python is cool";
+const text_3 = "JavaScript is amazing";
+
+console.log(text_1);
+console.log(text_2);
+console.log(text_3);
