15565206cd0d577a5ab9647fb8d82df476a16f3a
Enoch1357 <enochmensah1357@gmail.com>
2023-09-27 20:35:37 +0000

    1-starwars_title.js update

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -2,7 +2,6 @@
 // This script prints the title of a Star Wars movie where the episode number matches a given integer.
 const request = require('request');
 
-// Get the movie ID from command line argument
 const movieId = process.argv[2];
 
 if (!movieId) {
@@ -10,10 +9,8 @@ if (!movieId) {
   process.exit(1);
 }
 
-// Define the Star Wars API endpoint
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-// Make a GET request to the API
 request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
