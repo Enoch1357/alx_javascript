970997b7d7b712c5e5ef5c015be7954d52b02f26
Enoch1357 <enochmensah1357@gmail.com>
2023-10-08 17:13:21 +0000

    103-object_fct.js update

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -4,12 +4,19 @@ const myObject = {
     value: 12
   };
   console.log(myObject);
+
   myObject.incr = function() {
     this.value++;
-  }  
+  }
+
+  myObject.toString = (value) => {
+    value++;
+    return `{ type: 'object', value: ${value}, incr: [Function] }`;
+  }
+
   myObject.incr();
-  console.log(myObject);
+  console.log(myObject.toString(12));
   myObject.incr();
-  console.log(myObject);
+  console.log(myObject.toString(13));
   myObject.incr();
-  console.log(myObject);
+  console.log(myObject.toString(14));
